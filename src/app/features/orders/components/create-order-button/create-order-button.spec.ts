0679a0fc2d55e1202ebf8507b82929eb7337d128
Hector Escobedo <hescobedo@apexsystems.com>
2025-11-19 01:59:38 -0600

    test(config): fix zoneless change detection in test specs
    Add provideZonelessChangeDetection to test configurations to support Angular zoneless architecture.
    
    - Add provideZonelessChangeDetection import to Orders spec
    - Add provideZonelessChangeDetection import to CreateOrderButton spec
    - Update app.spec.ts test to check for router-outlet instead of title
    - Remove misconfigured dorny/test-reporter from CI pipeline
    
    All tests now pass successfully with zoneless change detection enabled.

--- src/app/features/orders/components/create-order-button/create-order-button.spec.ts
+++ src/app/features/orders/components/create-order-button/create-order-button.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideZonelessChangeDetection } from '@angular/core';
 import { CreateOrderButton } from './create-order-button';
 
 describe('CreateOrderButton', () => {
@@ -8,7 +8,8 @@ describe('CreateOrderButton', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateOrderButton]
+      imports: [CreateOrderButton],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
